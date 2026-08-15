897b4ab474546210e7ae213fa2ae9e3315eb439d
aarusharyal <aryalaarush203@gmail.com>
2026-08-15 23:25:18 +0545

    Minor Changes

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import authRoutes from "./routes/authRouter.js";
+import pageRoutes from "/routes/pageRouter.js";
 const app = express();
 
 app.use(express.json());
@@ -7,5 +9,7 @@ app.use(express.static("View/CSS"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(pageRoutes);
 
 export default app;
