4ec4503f0953d594af3907b3a74415064c443c4b
aarusharyal <aryalaarush203@gmail.com>
2026-08-19 22:08:17 +0545

    Added Some code

--- controller/pageController.js
+++ controller/pageController.js
@@ -14,3 +14,24 @@ export const getTodo = (req, res) => {
   const absoluteValue = path.resolve("./View/Html/todo.html");
   res.sendFile(absoluteValue);
 };
+
+export const apiTasks = (req, res) => {
+  const newTask = { completed: false, ...req.body };
+  console.log("Received task:", newTask);
+  fs.readFile("./model/tasks.json", "utf-8", (err, data) => {
+    let tasks = [];
+    if (!err && data) {
+      tasks = JSON.parse(data);
+    }
+    tasks.push(newTask);
+    fs.writeFile("./model/tasks.json", JSON.stringify(tasks), (err) => {
+      if (err) {
+        console.error("Error writing to file:", err);
+        res.status(500).send("Error Saving Task");
+      } else {
+        console.log("Task saved successfully.");
+        res.status(201).json(newTask);
+      }
+    });
+  });
+};
