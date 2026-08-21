5cb9c0adecbd97d13f32f0afdbec5d5f70d9d7d2
aarusharyal <aryalaarush203@gmail.com>
2026-08-21 21:48:16 +0545

    minor changes

--- index.js
+++ index.js
@@ -6,48 +6,43 @@ import fs from "fs";
 
 const PORT = 3000;
 
-// app.get("/",
-// app.get("/login.html"
-// app.get("/register.html"
-// app.get("/todo.html"
-
 app.get("/tasks.json", (req, res) => {
-  fs.readFile("tasks.json", "utf-8", (err, data) => {
-    if (err) {
-      if (err.code === "ENOENT") {
-        return res.json([]);
-      }
-      console.error("Error Reading File:", err);
-      return res.status(500).send("Error Reading File");
-    }
-    res.json(data ? JSON.parse(data) : []);
-  });
+  // fs.readFile("tasks.json", "utf-8", (err, data) => {
+  //   if (err) {
+  //     if (err.code === "ENOENT") {
+  //       return res.json([]);
+  //     }
+  //     console.error("Error Reading File:", err);
+  //     return res.status(500).send("Error Reading File");
+  //   }
+  //   res.json(data ? JSON.parse(data) : []);
+  // });
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-app.post("/api/tasks", (req, res) => {
-  const newTask = { completed: false, ...req.body };
-  console.log("Received task:", newTask);
-  fs.readFile("./model/tasks.json", "utf-8", (err, data) => {
-    let tasks = [];
-    if (!err && data) {
-      tasks = JSON.parse(data);
-    }
-    tasks.push(newTask);
-    fs.writeFile("./model/tasks.json", JSON.stringify(tasks), (err) => {
-      if (err) {
-        console.error("Error writing to file:", err);
-        res.status(500).send("Error Saving Task");
-      } else {
-        console.log("Task saved successfully.");
-        res.status(201).json(newTask);
-      }
-    });
-  });
-});
+// app.post("/api/tasks", (req, res) => {
+//   // const newTask = { completed: false, ...req.body };
+//   // console.log("Received task:", newTask);
+//   // fs.readFile("./model/tasks.json", "utf-8", (err, data) => {
+//   //   let tasks = [];
+//   //   if (!err && data) {
+//   //     tasks = JSON.parse(data);
+//   //   }
+//   //   tasks.push(newTask);
+//   //   fs.writeFile("./model/tasks.json", JSON.stringify(tasks), (err) => {
+//   //     if (err) {
+//   //       console.error("Error writing to file:", err);
+//   //       res.status(500).send("Error Saving Task");
+//   //     } else {
+//   //       console.log("Task saved successfully.");
+//   //       res.status(201).json(newTask);
+//   //     }
+//   //   });
+//   // });
+// });
 
 app.patch("/api/tasks/:id", (req, res) => {
   const { id } = req.params;
