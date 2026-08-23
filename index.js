0f31e38e646c748eb5cd978d38097c7235d71bbe
aarusharyal <aryalaarush203@gmail.com>
2026-08-23 23:07:11 +0545

    Changed Minor Codes and installed express-session

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
-import express from "express";
 import app from "./app.js";
-import http from "http";
-import path from "path";
 import fs from "fs";
 
 const PORT = 3000;
 
 app.get("/tasks.json", (req, res) => {
-  // fs.readFile("tasks.json", "utf-8", (err, data) => {
-  //   if (err) {
-  //     if (err.code === "ENOENT") {
-  //       return res.json([]);
-  //     }
-  //     console.error("Error Reading File:", err);
-  //     return res.status(500).send("Error Reading File");
-  //   }
-  //   res.json(data ? JSON.parse(data) : []);
-  // });
+  fs.readFile("./model/tasks.json", "utf-8", (err, data) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        return res.json([]);
+      }
+      console.error("Error Reading File:", err);
+      return res.status(500).send("Error Reading File");
+    }
+    res.json(data ? JSON.parse(data) : []);
+  });
 });
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-// app.post("/api/tasks", (req, res) => {
-//   // const newTask = { completed: false, ...req.body };
-//   // console.log("Received task:", newTask);
-//   // fs.readFile("./model/tasks.json", "utf-8", (err, data) => {
-//   //   let tasks = [];
-//   //   if (!err && data) {
-//   //     tasks = JSON.parse(data);
-//   //   }
-//   //   tasks.push(newTask);
-//   //   fs.writeFile("./model/tasks.json", JSON.stringify(tasks), (err) => {
-//   //     if (err) {
-//   //       console.error("Error writing to file:", err);
-//   //       res.status(500).send("Error Saving Task");
-//   //     } else {
-//   //       console.log("Task saved successfully.");
-//   //       res.status(201).json(newTask);
-//   //     }
-//   //   });
-//   // });
-// });
 
 app.patch("/api/tasks/:id", (req, res) => {
   const { id } = req.params;
@@ -67,4 +39,8 @@ app.patch("/api/tasks/:id", (req, res) => {
       res.json(task);
     });
   });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
