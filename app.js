90b3abcd03070e27654ea8fc3d646b39d34a491d
aarusharyal <aryalaarush203@gmail.com>
2026-08-23 23:06:22 +0545

    Changed App.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import express from "express";
+import { sessionMiddleware } from "./config/session.js";
 import authRoutes from "./routes/authRouter.js";
 import pageRoutes from "./routes/pageRouter.js";
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("View/JS"));
 app.use(express.static("View/CSS"));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(sessionMiddleware);
 
+app.use(authRoutes);
 app.use(pageRoutes);
 
 export default app;
