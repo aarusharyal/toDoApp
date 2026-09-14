297ffdabbace052165380427d7af2cdd293548a0
aarusharyal <aryalaarush203@gmail.com>
2026-09-14 21:14:40 +0545

    Updated RequireAuth

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,7 +1,13 @@
-export const requireAuth = (req,res,next) =>
-{
-  if(!req.session?.user){
-    return res.redirect("./login");
+export const requireAuth = (req, res, next) => {
+  if (!req.session?.user) {
+    return res.redirect("/login");
+  }
+  next();
+};
+
+export const requireAuthApi = (req, res, next) => {
+  if (!req.session?.user) {
+    return res.status(401).json({ message: "Not authenticated" });
   }
   next();
 };
